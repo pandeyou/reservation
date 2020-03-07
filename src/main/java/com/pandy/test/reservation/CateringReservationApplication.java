b6d3f35d7cb423fec13d5c105394ad4a9577ad83
pandy <33036894@qq.com>
2020-03-07 20:19:01 +0800

    处室化boot工程。

---
+++ src/main/java/com/pandy/test/reservation/CateringReservationApplication.java
@@ -0,0 +1,13 @@
+package com.pandy.test.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CateringReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CateringReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pandy/test/reservation/CateringReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandy.test.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CateringReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
